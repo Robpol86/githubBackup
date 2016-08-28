d20a6b69aa9a2d1757e1e5e3c339064444922808
Robpol86 <robpol86@gmail.com>
2016-08-28 13:58:13 -0500

    git init --bare

--- git/clone_test.go
+++ git/clone_test.go
@@ -1,9 +1,11 @@
 package git
 
 import (
+	"bytes"
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -14,6 +16,14 @@ func gitRemoteRepo() (string, func()) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	cmd := exec.Command("git", "init", "--bare")
+	cmd.Dir = dir
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	if err := cmd.Run(); err != nil {
+		log.Print(out)
+		log.Fatal(err)
+	}
 	return dir, func() { os.RemoveAll(dir) }
 }
 
@@ -21,4 +31,5 @@ func TestClone(t *testing.T) {
 	assert := require.New(t)
 	dir, clean := gitRemoteRepo()
 	defer clean()
+	assert.NotEmpty(dir) // TODO
 }
