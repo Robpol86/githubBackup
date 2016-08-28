898c676007f7de691b42fc952505736450df968d
Robpol86 <robpol86@gmail.com>
2016-08-28 14:23:54 -0500

    a lot

--- git/clone_test.go
+++ git/clone_test.go
@@ -11,53 +11,77 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func gitRemoteRepo() (string, func()) {
-	dir, err := ioutil.TempDir("", "gitRemote")
-	if err != nil {
-		log.Fatal(err)
-	}
-	cmd := exec.Command("git", "init", "--bare")
-	cmd.Dir = dir
+func run(cwd, name string, arg ...string) *bytes.Buffer {
+	cmd := exec.Command(name, arg)
+	cmd.Dir = cwd
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	if err := cmd.Run(); err != nil {
 		log.Print(out)
 		log.Fatal(err)
 	}
-	return dir, func() { os.RemoveAll(dir) }
+	return &out
 }
 
-func TestClone_Simple(t *testing.T) {
-	assert := require.New(t)
-	dir, clean := gitRemoteRepo()
-	defer clean()
-	assert.NotEmpty(dir) // TODO
+func gitRemoteRepo(stop string) (string, func()) {
+	remoteDir, err := ioutil.TempDir("", "gitRemote")
+	if err != nil {
+		log.Fatal(err)
+	}
+	clean := func() { os.RemoveAll(remoteDir) }
+	if stop == "doesn't exist" {
+		clean()
+		return remoteDir, func() {}
+	}
+
+	// Create remote repo (locally).
+	run(remoteDir, "git", "init", "--bare")
+	if stop == "no commits" {
+		return remoteDir, clean
+	}
+
+	// Commit files to it.
+	localDir, err := ioutil.TempDir("", "gitRemoteLocal")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.RemoveAll(localDir)
+	run(localDir, "git", "init")
+	run(localDir, "git", "remote", "add", "origin", remoteDir)
+	// ioutil.WriteFile(os.)
+
+	return remoteDir, clean
 }
 
-func TestClone_TagsBranchesRemoted(t *testing.T) {
+func TestClone_NetworkError(t *testing.T) {
 	assert := require.New(t)
-	dir, clean := gitRemoteRepo()
+	assert.NotEmpty("TODO") // TODO
+}
+
+func TestClone_RepoDoesntExist(t *testing.T) {
+	assert := require.New(t)
+	dir, clean := gitRemoteRepo("doesn't exist")
 	defer clean()
 	assert.NotEmpty(dir) // TODO
 }
 
 func TestClone_NoCommits(t *testing.T) {
 	assert := require.New(t)
-	dir, clean := gitRemoteRepo()
+	dir, clean := gitRemoteRepo("no commits")
 	defer clean()
 	assert.NotEmpty(dir) // TODO
 }
 
-func TestClone_RepoDoesntExist(t *testing.T) {
+func TestClone_Simple(t *testing.T) {
 	assert := require.New(t)
-	dir, clean := gitRemoteRepo()
+	dir, clean := gitRemoteRepo("simple")
 	defer clean()
 	assert.NotEmpty(dir) // TODO
 }
 
-func TestClone_NetworkError(t *testing.T) {
+func TestClone_TagsBranchesRemoted(t *testing.T) {
 	assert := require.New(t)
-	dir, clean := gitRemoteRepo()
+	dir, clean := gitRemoteRepo("")
 	defer clean()
 	assert.NotEmpty(dir) // TODO
 }
