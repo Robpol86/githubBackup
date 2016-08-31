27e3edccd0b18e3c79f2a32550a4af9500202c6e
Robpol86 <robpol86@gmail.com>
2016-08-31 12:01:07 -0500

    cli hello world

--- githubBackup.go
+++ githubBackup.go
@@ -1,8 +1,32 @@
 package main
 
 import (
+	"os"
+
 	log "github.com/Sirupsen/logrus"
+	"gopkg.in/urfave/cli.v2"
 )
+
+// init just handles CLI parsing and populating the global config.
+func init() {
+	flags := []cli.Flag{
+		&cli.StringFlag{
+			Name:    "lang",
+			Aliases: []string{"l"},
+			Value:   "english",
+			Usage:   "language for the greeting",
+		}}
+
+	app := &cli.App{
+		Action: func (c *cli.Context) error {
+			log.Info("Hello from cli.")
+			return nil
+		},
+		Flags: flags,
+	}
+
+	app.Run(os.Args)
+}
 
 func main() {
 	log.Debug("Debug.")
