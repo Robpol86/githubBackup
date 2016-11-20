1e55b8767e3c5f7f1882964083a93182bdaa6ccb
Robpol86 <robpol86@gmail.com>
2016-11-20 14:22:00 -0800

    GetRepos stub

--- api/repos.go
+++ api/repos.go
@@ -1,1 +1,14 @@
 package api
+
+import "github.com/google/go-github/github"
+
+// GetRepos retrieves the list of public and optionall private GitHub repos on the user's account.
+//
+// :param user: Get repositories for this user. If blank username is derived from token.
+//
+// :param token: API token for authentication. Required if user is blank.
+//
+// :param private: Also lookup private repositories. Requires API token.
+func GetRepos(user, token string, private bool) []*github.Repository {
+	return nil
+}
