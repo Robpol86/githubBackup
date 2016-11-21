4df681c8cca3228d5cc497c038a64e3dbe31d306
Robpol86 <robpol86@gmail.com>
2016-11-20 16:08:51 -0800

    wip

--- api/repos.go
+++ api/repos.go
@@ -2,19 +2,35 @@ package api
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
+
+	"github.com/Robpol86/githubBackup/config"
 )
 
-// GetRepos retrieves the list of public and optionall private GitHub repos on the user's account.
+// Repository represents one GitHub repository in API responses.
+type Repository struct {
+	Name     string
+	Private  bool
+	Fork     bool
+	GitURL   string
+	Size     int
+	PushedAt time.Time
+}
+
+// GetRepos retrieves the list of public and optionall private GitHub repos on the user's account. // TODO toggle forks.
 //
 // :param user: Get repositories for this user. If blank username is derived from token.
 //
 // :param token: API token for authentication. Required if user is blank.
 //
 // :param private: Also lookup private repositories. Requires API token.
-func GetRepos(user, token string, private bool) (repos []*github.Repository, err error) {
+func GetRepos(user, token string, private bool) (repositories []Repository, err error) {
+	log := config.GetLogger()
+
+	// Setup HTTP client.
 	var httpClient *http.Client
 	if token != "" {
 		tokenSource := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
@@ -29,7 +45,24 @@ func GetRepos(user, token string, private bool) (repos []*github.Repository, err
 	}
 
 	// Query API.
-	repos, _, err = client.Repositories.List(user, options)
+	repos, response, err := client.Repositories.List(user, options)
+	log.Debugf("GitHub API response: %v", response)
+	if err != nil {
+		log.Errorf("Failed to query for repos: %s", err.Error())
+		return
+	}
+
+	// Parse.
+	for _, repo := range repos {
+		repositories = append(repositories, Repository{
+			Name:     *repo.Name,
+			Private:  *repo.Private,
+			Fork:     *repo.Fork,
+			GitURL:   *repo.GitURL,
+			Size:     *repo.Size,
+			PushedAt: repo.PushedAt.Time,
+		})
+	}
 
 	return
 }
