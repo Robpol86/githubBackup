93ee5f1d9f2a432ee37570342f0129ceaefee57d
Robpol86 <robpol86@gmail.com>
2016-11-21 23:14:10 -0800

    combined

--- api/repos_test.go
+++ api/repos_test.go
@@ -11,55 +11,63 @@ import (
 	"github.com/Robpol86/githubBackup/testUtils"
 )
 
-func TestGetReposBadAuth(t *testing.T) {
-	assert := require.New(t)
+func TestGetReposBad(t *testing.T) {
+	reply := map[string]string{
+		"auth": `{"message": "Bad credentials", "documentation_url": ""}`,
+		"user": `{"message": "Not Found", "documentation_url": ""}`,
+	}
+	replyCode := map[string]int{
+		"auth": 401,
+		"user": 404,
+	}
+	errorMsg := map[string]string{
+		"auth": "GET %s/user/repos: 401 Bad credentials []",
+		"user": "GET %s/users/unknown/repos: 404 Not Found []",
+	}
+	contains := map[string]string{
+		"auth": "Failed to query for repos: GET %s/user/repos: 401 Bad credentials",
+		"user": "Failed to query for repos: GET %s/users/unknown/repos: 404 Not Found",
+	}
+	user := map[string]string{
+		"auth": "",
+		"user": "unknown",
+	}
+	token := map[string]string{
+		"auth": "bad token",
+		"user": "",
+	}
 
-	// Verify HTTP request.
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		value, ok := r.Header["Authorization"]
-		assert.True(ok)
-		assert.Equal([]string{"Bearer bad token"}, value)
-		w.WriteHeader(401)
-		w.Write([]byte(`{"message": "Bad credentials", "documentation_url": ""}`))
-	}))
-	defer ts.Close()
+	for _, bad := range []string{"auth", "user"} {
+		t.Run(bad, func(t *testing.T) {
+			assert := require.New(t)
 
-	// Run.
-	stdout, stderr, err := testUtils.WithCapSys(func() {
-		testUtils.ResetLogger()
-		repos, err := GetRepos("", "bad token", ts.URL, false, false, false)
-		assert.EqualError(err, fmt.Sprintf("GET %s/user/repos: 401 Bad credentials []", ts.URL))
-		assert.Empty(repos)
-	})
+			// Verify HTTP request.
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				value, ok := r.Header["Authorization"]
+				if bad == "auth" {
+					assert.True(ok)
+					assert.Equal([]string{"Bearer bad token"}, value)
+				} else {
+					assert.False(ok)
+				}
+				w.WriteHeader(replyCode[bad])
+				w.Write([]byte(reply[bad]))
+			}))
+			defer ts.Close()
 
-	// Verify log.
-	assert.NoError(err)
-	assert.Empty(stdout)
-	assert.Contains(stderr, fmt.Sprintf("Failed to query for repos: GET %s/user/repos: 401 Bad credential", ts.URL))
-}
+			// Run.
+			stdout, stderr, err := testUtils.WithCapSys(func() {
+				testUtils.ResetLogger()
+				repos, err := GetRepos(user[bad], token[bad], ts.URL, false, false, false)
+				assert.EqualError(err, fmt.Sprintf(errorMsg[bad], ts.URL))
+				assert.Empty(repos)
+			})
 
-func TestGetReposBadUser(t *testing.T) {
-	assert := require.New(t)
+			// Verify log.
+			assert.NoError(err)
+			assert.Empty(stdout)
+			assert.Contains(stderr, fmt.Sprintf(contains[bad], ts.URL))
+		})
+	}
 
-	// Verify HTTP request.
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		_, ok := r.Header["Authorization"]
-		assert.False(ok)
-		w.WriteHeader(404)
-		w.Write([]byte(`{"message": "Not Found", "documentation_url": ""}`))
-	}))
-	defer ts.Close()
-
-	// Run.
-	stdout, stderr, err := testUtils.WithCapSys(func() {
-		testUtils.ResetLogger()
-		repos, err := GetRepos("unknown", "", ts.URL, false, false, false)
-		assert.EqualError(err, fmt.Sprintf("GET %s/users/unknown/repos: 404 Not Found []", ts.URL))
-		assert.Empty(repos)
-	})
-
-	// Verify log.
-	assert.NoError(err)
-	assert.Empty(stdout)
-	assert.Contains(stderr, fmt.Sprintf("Failed to query for repos: GET %s/users/unknown/repos: 404 Not F", ts.URL))
 }
