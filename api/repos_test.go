80310250f326210fa50c7cdc3578338466bab92d
Robpol86 <robpol86@gmail.com>
2016-11-21 22:42:43 -0800

    TestGetReposBadUser

--- api/repos_test.go
+++ api/repos_test.go
@@ -37,3 +37,29 @@ func TestGetReposBadAuth(t *testing.T) {
 	assert.Empty(stdout)
 	assert.Contains(stderr, fmt.Sprintf("Failed to query for repos: GET %s/user/repos: 401 Bad credential", ts.URL))
 }
+
+func TestGetReposBadUser(t *testing.T) {
+	assert := require.New(t)
+
+	// Verify HTTP request.
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, ok := r.Header["Authorization"]
+		assert.False(ok)
+		w.WriteHeader(404)
+		w.Write([]byte(`{"message": "Not Found", "documentation_url": ""}`))
+	}))
+	defer ts.Close()
+
+	// Run.
+	stdout, stderr, err := testUtils.WithCapSys(func() {
+		testUtils.ResetLogger()
+		repos, err := GetRepos("unknown", "", ts.URL, false, false, false)
+		assert.EqualError(err, fmt.Sprintf("GET %s/users/unknown/repos: 404 Not Found []", ts.URL))
+		assert.Empty(repos)
+	})
+
+	// Verify log.
+	assert.NoError(err)
+	assert.Empty(stdout)
+	assert.Contains(stderr, fmt.Sprintf("Failed to query for repos: GET %s/users/unknown/repos: 404 Not F", ts.URL))
+}
