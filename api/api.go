7c083c2249227cc3089adb6589ca99b537fbc515
Robpol86 <robpol86@gmail.com>
2016-11-24 13:23:33 -0800

    TODO

--- api/api.go
+++ api/api.go
@@ -11,12 +11,28 @@ type API struct {
 	NoPublic  bool
 	NoWikis   bool
 	Token     string
-	URL       string
 	User      string
+
+	TestPrompt string
+	TestURL    string
 }
 
 // NewAPI reads config data and conditionally prompts for the API token (as a password prompt).
 func NewAPI(config config.Config) API {
+	// Determine if we should prompt user for token. Always prompt (optional or mandatory regardless) for token if
+	// not specified in --token since there are higher API limits for authenticated users.
+	token, optional, mandatory := config.Token, config.Token == "", config.Token == "" && config.User == ""
+	if token == "" && optional && mandatory {
+		// TODO
+
+		/*
+			NoPrivate bool
+			Token     string
+			NoPrompt  bool
+			User      string
+		*/
+	}
+
 	return API{
 		NoForks:   config.NoForks,
 		NoPrivate: config.NoPrivate,
