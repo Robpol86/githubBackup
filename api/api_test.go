02fad21d4ea8736e61250b82458c6248e8a12c2b
Robpol86 <robpol86@gmail.com>
2016-11-25 15:44:35 -0800

    one less variable

--- api/api_test.go
+++ api/api_test.go
@@ -11,8 +11,7 @@ import (
 
 func TestNewAPIWithToken(t *testing.T) {
 	assert := require.New(t)
-	cfg := config.Config{Token: "abc"}
-	api, err := NewAPI(cfg, "xyz")
+	api, err := NewAPI(config.Config{Token: "abc"}, "xyz")
 	assert.NoError(err)
 	assert.Equal("abc", api.Token)
 }
@@ -21,9 +20,8 @@ func TestNewAPINoPrompt(t *testing.T) {
 	for _, user := range []string{"me", ""} {
 		t.Run(user, func(t *testing.T) {
 			assert := require.New(t)
-			cfg := config.Config{NoPrompt: true, User: user}
 			logs, stdout, stderr, err := testUtils.WithLogging(func() {
-				api, err := NewAPI(cfg, "xyz")
+				api, err := NewAPI(config.Config{NoPrompt: true, User: user}, "xyz")
 				if user == "" {
 					assert.EqualError(err, "no token or user given, unable to query")
 				} else {
