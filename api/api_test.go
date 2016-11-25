cca7625cffeb1a79a455991fe10475190e63ffb2
Robpol86 <robpol86@gmail.com>
2016-11-25 15:36:19 -0800

    done with no prompt

--- api/api_test.go
+++ api/api_test.go
@@ -18,17 +18,23 @@ func TestNewAPIWithToken(t *testing.T) {
 }
 
 func TestNewAPINoPrompt(t *testing.T) {
-	t.Run("withUser", func(t *testing.T) {
-		assert := require.New(t)
-		cfg := config.Config{NoPrompt: true, User: "me"}
-		logs, stdout, stderr, err := testUtils.WithLogging(func() {
-			api, err := NewAPI(cfg, "xyz")
+	for _, user := range []string{"me", ""} {
+		t.Run(user, func(t *testing.T) {
+			assert := require.New(t)
+			cfg := config.Config{NoPrompt: true, User: user}
+			logs, stdout, stderr, err := testUtils.WithLogging(func() {
+				api, err := NewAPI(cfg, "xyz")
+				if user == "" {
+					assert.EqualError(err, "no token or user given, unable to query")
+				} else {
+					assert.NoError(err)
+				}
+				assert.Equal("", api.Token)
+			})
+			assert.Len(logs.Entries, 0)
+			assert.Empty(stdout)
+			assert.Empty(stderr)
 			assert.NoError(err)
-			assert.Equal("", api.Token)
 		})
-		assert.Len(logs.Entries, 0)
-		assert.Empty(stdout)
-		assert.Empty(stderr)
-		assert.NoError(err)
-	})
+	}
 }
