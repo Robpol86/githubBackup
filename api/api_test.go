673466252f9da7ea3c5a1cb6eb4725fdddf6f2ee
Robpol86 <robpol86@gmail.com>
2016-11-25 13:21:43 -0800

    Easy test

--- api/api_test.go
+++ api/api_test.go
@@ -4,10 +4,14 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+
+	"github.com/Robpol86/githubBackup/config"
 )
 
 func TestNewAPIWithToken(t *testing.T) {
 	assert := require.New(t)
-
-	assert.True(true) // TODO.
+	cfg := config.Config{Token: "abc"}
+	api, err := NewAPI(cfg, "")
+	assert.NoError(err)
+	assert.Equal("abc", api.Token)
 }
