e2cece3e25e3d186c282b5afd4557ab86efffb1a
Robpol86 <robpol86@gmail.com>
2016-11-25 18:17:30 -0800

    validErrors

--- api/api_test.go
+++ api/api_test.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/Sirupsen/logrus"
@@ -61,21 +62,40 @@ func TestNewAPIPrompt(t *testing.T) {
 }
 
 func TestNewAPIError(t *testing.T) {
+	validErrors := [...]string{
+		"operation not supported by device",
+		"inappropriate ioctl for device",
+	}
+
 	for _, user := range []string{"me", ""} {
 		t.Run(user, func(t *testing.T) {
 			assert := require.New(t)
+
 			logs, stdout, stderr, err := testUtils.WithLogging(func() {
 				api, err := NewAPI(config.Config{User: user}, "")
 				if user == "" {
-					m := "failed reading stdin for token: operation not supported by device"
-					assert.EqualError(err, m)
+					var i int
+					for i = 0; i < len(validErrors); i++ {
+						if strings.HasSuffix(err.Error(), validErrors[i]) {
+							break
+						}
+					}
+					assert.EqualError(err, "failed reading stdin for token: "+validErrors[i])
 				} else {
 					assert.NoError(err)
 				}
 				assert.Equal("", api.Token)
 			})
+
 			assert.Len(logs.Entries, 2)
-			assert.Equal("operation not supported by device", logs.LastEntry().Message)
+			var i int
+			for i = 0; i < len(validErrors); i++ {
+				if strings.HasSuffix(logs.LastEntry().Message, validErrors[i]) {
+					break
+				}
+			}
+			assert.Equal(validErrors[i], logs.LastEntry().Message)
+
 			if user == "" {
 				assert.Equal("Enter your GitHub personal access token: failed to read stdin\n", stdout)
 			} else {
