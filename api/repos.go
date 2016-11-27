1b1ac82be2ec4c1232986475f91f5407816a4084
Robpol86 <robpol86@gmail.com>
2016-11-27 13:29:59 -0800

    split

--- api/repos.go
+++ api/repos.go
@@ -9,6 +9,51 @@ import (
 	"github.com/Robpol86/githubBackup/config"
 )
 
+func (a *API) parseRepo(repo *github.Repository, tasks Tasks) {
+	// Create task.
+	task := Task{
+		Name:     *repo.Name,
+		Private:  *repo.Private,
+		PushedAt: repo.PushedAt.Time,
+		Size:     *repo.Size,
+
+		CloneURL: *repo.CloneURL,
+	}
+	if *repo.Private {
+		task.CloneURL = *repo.SSHURL
+	}
+
+	// Add task.
+	dir := tasks.validDir(task.Name)
+	tasks[dir] = task
+
+	// Add wiki as a separate repo.
+	if !a.NoWikis && *repo.HasWiki {
+		wikiTask := task.dup()
+		wikiTask.IsWiki = true
+		wikiTask.Name += ".wiki"
+		wikiTask.CloneURL = task.CloneURL[:len(task.CloneURL)-4] + ".wiki.git"
+		tasks[tasks.validDir(dir+".wiki")] = wikiTask
+	}
+
+	// Add issues.
+	if !a.NoIssues && *repo.HasIssues {
+		issueTask := task.dup()
+		issueTask.Name += ".issues"
+		issueTask.JustIssues = true
+		tasks[tasks.validDir(dir+".issues")] = issueTask
+	}
+
+	// Add releases.
+	if !a.NoReleases {
+		// Nothing in API response to indicate if repo has releases. Assuming yes for all repos for now.
+		releasesTask := task.dup()
+		releasesTask.Name += ".releases"
+		releasesTask.JustReleases = true
+		tasks[tasks.validDir(dir+".releases")] = releasesTask
+	}
+}
+
 // GetRepos retrieves the list of public and private GitHub repos on the user's account.
 //
 // :param tasks: Already-initialized Tasks map to add tasks to.
@@ -40,49 +85,7 @@ func (a *API) GetRepos(tasks Tasks) error {
 		if (a.NoForks && *repo.Fork) || (a.NoPublic && !*repo.Private) || (a.NoPrivate && *repo.Private) {
 			continue
 		}
-
-		// Create task.
-		task := Task{
-			Name:     *repo.Name,
-			Private:  *repo.Private,
-			PushedAt: repo.PushedAt.Time,
-			Size:     *repo.Size,
-
-			CloneURL: *repo.CloneURL,
-		}
-		if *repo.Private {
-			task.CloneURL = *repo.SSHURL
-		}
-
-		// Add task.
-		dir := tasks.validDir(task.Name)
-		tasks[dir] = task
-
-		// Add wiki as a separate repo.
-		if !a.NoWikis && *repo.HasWiki {
-			wikiTask := task.dup()
-			wikiTask.IsWiki = true
-			wikiTask.Name += ".wiki"
-			wikiTask.CloneURL = task.CloneURL[:len(task.CloneURL)-4] + ".wiki.git"
-			tasks[tasks.validDir(dir+".wiki")] = wikiTask
-		}
-
-		// Add issues.
-		if !a.NoIssues && *repo.HasIssues {
-			issueTask := task.dup()
-			issueTask.Name += ".issues"
-			issueTask.JustIssues = true
-			tasks[tasks.validDir(dir+".issues")] = issueTask
-		}
-
-		// Add releases.
-		if !a.NoReleases {
-			// Nothing in API response to indicate if repo has releases. Assuming yes for all repos for now.
-			releasesTask := task.dup()
-			releasesTask.Name += ".releases"
-			releasesTask.JustReleases = true
-			tasks[tasks.validDir(dir+".releases")] = releasesTask
-		}
+		a.parseRepo(repo, tasks)
 	}
 
 	return nil
