eec2f083d890df98bc33ac0f048d435cb7e2ccfd
Robpol86 <robpol86@gmail.com>
2016-12-11 15:18:26 -0800

    TestMainReposAPIError

--- main_test.go
+++ main_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"bufio"
 	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"path/filepath"
 	"regexp"
@@ -91,4 +93,26 @@ func TestMainTokenError(t *testing.T) {
 	assert.Contains(stderr, "Not querying GitHub API: ")
 }
 
-// TODO: Test api error and empty response. Add testing argument to Main().
+func TestMainReposAPIError(t *testing.T) {
+	assert := require.New(t)
+
+	tmpdir, err := ioutil.TempDir("", "")
+	assert.NoError(err)
+	defer os.RemoveAll(tmpdir)
+	defer testUtils.ResetLogger()
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		w.Write([]byte("{':"))
+	}))
+	defer ts.Close()
+
+	stdout, stderr, err := testUtils.WithCapSys(func() {
+		testUtils.ResetLogger()
+		ret := Main([]string{"-TuRobpol86", tmpdir}, ts.URL)
+		assert.Equal(1, ret)
+	})
+
+	assert.NoError(err)
+	assert.Contains(stdout, "githubBackup "+config.Version)
+	assert.Contains(stderr, "Querying GitHub API for repositories failed")
+}
